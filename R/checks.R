1544be7f7ae191c770ae9c1577f7385f8948b395
VANDERBILT\shress6 <shristi.shrestha.2@vanderbilt.edu>
2022-07-28 10:59:25 -0500

    edits to checks

--- R/checks.R
+++ R/checks.R
@@ -21,14 +21,16 @@ Is_Seurat <- function(
 #'
 #' Check to see if cell type metadata column name exist in the seurat object, if not returns an error
 #'
-#' @param cell_type_colname Seurat object name.
+#' @param seurat_object Seurat object name.
+#' @param cell_type_colname cell type column name in metadata
 #' @return returns an error message if cell_type_colname does not exist
 
-meta_col_names <- colnames(x = seurat_object@meta.data)
+
 
 Is_celltype_colname <- function(
   seurat_object
 ) {
+  meta_col_names <- colnames(x = seurat_object@meta.data)
   if (cell_type_colname %in% meta_col_names == "FALSE") {
     stop("Entered 'cell_type_colname' not found in object metadata")
   }
@@ -42,14 +44,16 @@ Is_celltype_colname <- function(
 #' Check to see if cell type  name exist in the seurat object, if not returns an error
 #'
 #' @param seurat_object Seurat object name.
+#' @param cell_type_name cell type name to highlight
 #' @return returns an error message if cell type name does not exist
 
-meta <- seurat_object@meta.data
-cell_type_column <- meta[,cell_type_colname]
+
 
 Is_cell_type_name <- function(
   seurat_object
 ) {
+  meta <- seurat_object@meta.data
+  cell_type_column <- meta[,cell_type_colname]
   if (cell_type_name %in% cell_type_column == "FALSE") {
     stop("Entered 'cell_type_name' not found in the 'cell_type_colname' of object metadata")
   }
@@ -61,6 +65,7 @@ Is_cell_type_name <- function(
 #' Check if 'meta_group' exists
 #'
 #' @param seurat_object Seurat object name.
+#' @param meta_group name of group to split visuals by
 #' @return returns an error message if meta_group does not exist
 
 Is_meta_group <- function(
@@ -77,12 +82,14 @@ Is_meta_group <- function(
 #' Check if gene included in object
 #'
 #' @param seurat_object Seurat object name.
+#' @param gene name of gene
 #' @return returns an error message if gene is not found in seurat object
 
-gene_names <- rownames(seurat_object)
+
 Is_gene <- function(
   seurat_object
 ) {
+  gene_names <- rownames(seurat_object)
   if (gene %in% gene_names == "FALSE") {
     stop("Entered 'gene' in object not found")
   }
