3f089a2751a6676d9a4ce5fbf3c362e1983deee9
VANDERBILT\shress6 <shristi.shrestha.2@vanderbilt.edu>
2023-07-25 15:08:55 -0500

    consistent naming of 'Tableau'

--- R/GenePanel.R
+++ R/GenePanel.R
@@ -19,7 +19,11 @@
 #' @param cell_type_colname The metadata column name that contains the cell
 #'   identity annotations
 #' @param col_palette Color palettes to choose for violinplot panel. Options
+<<<<<<< Updated upstream
 #'   are "tableu" or RColorBrewer qualitative variables like "Dark2", "Paired",
+=======
+#'   are "Tableau" or RColorBrewer qualitative variables like "Dark2", "Paired",
+>>>>>>> Stashed changes
 #'   "Set1", "Set2", "Set3", "Accent" etc.
 #' @param group_order User defined order of the meta_groups to be displayed
 #' @param output_dir Output directory where the image will be saved
